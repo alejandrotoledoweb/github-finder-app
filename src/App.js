d78e62547d70b2af8cc387f032b0eb1b024cf1dc
alejandro toledo <atoledofr@gmail.com>
2022-02-07 11:14:55 -0500

    create home about notfound and user file with some basic information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
