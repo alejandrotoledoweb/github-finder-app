67e77def015de333452686f5608ee2fa13faad20
alejandro toledo <atoledofr@gmail.com>
2022-02-07 12:23:24 -0500

    add a fetch request to access some users from github and add a spinner component when loading is in progress

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import Spinner from '../layout/Spinner';
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`https://api.github.com/users`);
+
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 ls:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3 key={user.id}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
